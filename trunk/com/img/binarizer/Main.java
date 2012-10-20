9abd28c40eec5bf1fd142f2bdfe0a0f727799e61
chrhad081@gmail.com <chrhad081@gmail.com@0f82fece-e3fb-e6a6-36e8-3e2704e416a4>
2012-10-20 02:37:20 +0000

    Added: labeler/Labeler.java contains new class to print single label file out of multiple image inputs listed in a file labeler/Main.java is the (temporary) labeler executable. Some lines need to be updated
    Modified:
    extractor/LabelExtractor.java: [comment] a question in add(colorarray[0])
    binarizer/Main.java: more flexible directory allocation, proven to work, TODO: read from argument of text file containing image file names list

--- trunk/com/img/binarizer/Main.java
+++ trunk/com/img/binarizer/Main.java
@@ -26,19 +26,18 @@ public class Main {
 			int size=5;
 			FeatureExtractor fe=new DCTExtractor(5); //let the size of the block be 5x5.
 		    Path path = Paths.get(filepath);
-		    Path outputfolder=Paths.get("D:", path.getFileName().toString().split("\\.")[0]);// The "D:" can be change to any absolute path of a folder. The outputfolder is this case is D:\[filename]
+		    //Path outputfolder=Paths.get("D:", path.getFileName().toString().split("\\.")[0]);// The "D:" can be change to any absolute path of a folder. The outputfolder is this case is D:\[filename]
+		    Path outputfolder=Paths.get(filepath.split("\\.")[0]);// The "D:" can be change to any absolute path of a folder. The outputfolder is this case is D:\[filename]
 		   
 		    try(InputStream is=Files.newInputStream(path)){
 		    	Files.createDirectories(outputfolder);
 		    	BufferedImage image = ImageIO.read(is);
 		    	ScanLine[] features = fe.extract(image); //Note that the first floor(size/2) rows and columns are ommitted
 		    	
-		    	
-		    	
 		    	Binarizer bn=new Binarizer();
 		    	int startlineNum=1+size/2;//calculate the ommited boundary.
 		    	for(int i=0; i<features.length; i++){	//each line become one file.
-		    	 Path pix=Paths.get(outputfolder.toString(), String.format("line%d.pix", i+startlineNum));
+		    	 Path pix=Paths.get(outputfolder.toString(), String.format("line%04d.pix", i+startlineNum));
 		    	 bn.binarize(features[i], pix.toString());
 		    	}
 		    	
