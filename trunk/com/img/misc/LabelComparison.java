62441ee65f06922c6254fe8e04c0e19905800e8a
chrhad081@gmail.com <chrhad081@gmail.com@0f82fece-e3fb-e6a6-36e8-3e2704e416a4>
2012-11-28 14:06:16 +0000

    [minor] LabelComparison.java remove unused import

--- trunk/com/img/misc/LabelComparison.java
+++ trunk/com/img/misc/LabelComparison.java
@@ -9,11 +9,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-
 import javax.imageio.ImageIO;
 
 
